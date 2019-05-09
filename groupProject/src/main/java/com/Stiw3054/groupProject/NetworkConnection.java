e792746b1ed4dd6f2b490af457587adf59c86ba3
MEE WAI <38762366+MeeWai@users.noreply.github.com>
2019-05-10 01:03:29 +0800

    Update NetworkConnection.java

--- groupProject/src/main/java/com/Stiw3054/groupProject/NetworkConnection.java
+++ groupProject/src/main/java/com/Stiw3054/groupProject/NetworkConnection.java
@@ -3,8 +3,9 @@ package com.Stiw3054.groupProject;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.Callable;
 
-public class NetworkConnection extends Thread {
+public class NetworkConnection implements Callable<String> {
     private String url, path;
 
     NetworkConnection(String path, String url) {
@@ -17,26 +18,30 @@ public class NetworkConnection extends Thread {
         try {
             HttpURLConnection.setFollowRedirects(false);
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setConnectTimeout(1000);
-            connection.setReadTimeout(1000);
+            connection.setConnectTimeout(60000);
+            connection.setReadTimeout(60000);
             connection.setRequestMethod("HEAD");
             return (connection.getResponseCode() == HttpURLConnection.HTTP_OK);
         } catch (Exception e) {
             return false;
+
         }
     }
 
     @Override
-    public void run() {
+    public String call() throws Exception {
         if (checkURL(url)) {
-            System.out.println(Thread.currentThread().getName() + "-" + url + ": Exist");
+            //ystem.out.println(Thread.currentThread().getName() + "-" + url + ": Exist");
+
         } else{
             try {
                 LogFile logFile = new LogFile();
                 logFile.createLogFile(path, url);
+                url = "0";
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        return url;
     }
 }
