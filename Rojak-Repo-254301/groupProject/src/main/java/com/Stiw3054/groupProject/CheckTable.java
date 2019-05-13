4ea09f18da3226104a7fbb062d30cc7dc63e9ea9
Tan Shi Jet <34006871+TanShiJet@users.noreply.github.com>
2019-05-13 21:36:31 +0800

    Update CheckTable.java

--- Rojak-Repo-254301/groupProject/src/main/java/com/Stiw3054/groupProject/CheckTable.java
+++ Rojak-Repo-254301/groupProject/src/main/java/com/Stiw3054/groupProject/CheckTable.java
@@ -8,8 +8,11 @@ import java.io.IOException;
 public class CheckTable {
     List <String> ValidURLList;
     Document doc;
-    String tabletext = "";
     String xxx= "";
+    ObjectTable [] playerlist = new ObjectTable[10];
+    int countvtable = 0;
+    String cat;
+
 
     public CheckTable(List <String> ValidURLList){
 
@@ -20,29 +23,64 @@ public class CheckTable {
     public void Checktable () throws IOException {
 
         for (int x = 0; x < ValidURLList.size(); x++) {
+            int count = 0;
+            boolean ifvalid = false;
+
+            ObjectTable objectTable = new ObjectTable();
             doc = Jsoup.connect(ValidURLList.get(x)).get();
             Elements table = doc.select("table.CRs1 tr");
-            for (Element tr : table) {
-                Elements tds = tr.getElementsByTag("td");
-                if (tds.hasText()){
-                    Elements table2 = table.select("div.defaultDialog h2");
-                    for (Element tr1 : table2) {
-                        Elements tds1 = tr1.getElementsMatchingText("\\d{4}?");
-                        if (!xxx.equals(tds1.text())&&(tds1.hasText())){
-                            tabletext += tds1.text() + "\n";
-                            xxx = tds1.text();
-                            //System.out.println(tabletext);
+            for (Element row : table) {
+                Elements tds = row.getElementsByTag("td");
+                ifvalid =tds.hasText();
+               if (ifvalid) {
+                    Elements table2 = doc.select("div.defaultDialog h2");
+                    for (Element row1 : table2) {
+                        Elements tds1 = row1.getElementsMatchingText("\\d{4}?");
+                        if (tds1.hasText()){
+                            cat =tds1.text();
+                            //System.out.println(tds1.text());
                         }
 
                     }
-                    tabletext+=tds.text()+"\n";
+
+                    objectTable.setArrayRK(row.select("td:nth-child(1)").text(), count);//
+                    objectTable.setArraySno(row.select("td:nth-child(2)").text(),count);
+                    objectTable.setArrayName(row.select(" td:nth-child(4)").text(),count);
+                    objectTable.setArrayRtg( row.select("td.CRr").text(),count);
+                    objectTable.setArrayState(row.select("td:nth-child(7)").text(),count);
+                    objectTable.setArrayPts(row.select("td:nth-child(8)").text(),count);
+                    objectTable.setArrayCat(cat,count);
+                    count ++;
+
+
+
+
+
                 }
+                else{// enter log file
+
+                }
+
+
             }
+            //System.out.println(ifvalid);
+            if (ifvalid) {
+                playerlist[countvtable] = objectTable;
+                countvtable++;
+            }
+
+
+
         }
+
+        /*for(String ppp:playerlist[1].getArrayRK()){
+            System.out.println(ppp);
+        }*/
     }
 
-    public String getPlayerList() {
-        return tabletext;
+    public ObjectTable [] getPlayerList() {
+
+        return playerlist;
     }
 
 }
