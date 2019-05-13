9bf3cccaf48dee3f6595b6ff8a51ff58cf2e48e6
MEE WAI <38762366+MeeWai@users.noreply.github.com>
2019-05-14 00:16:20 +0800

    Update DisplayPlayerFromKedah.java

--- Rojak-Repo-254301/groupProject/src/main/java/com/Stiw3054/groupProject/DisplayPlayerFromKedah.java
+++ Rojak-Repo-254301/groupProject/src/main/java/com/Stiw3054/groupProject/DisplayPlayerFromKedah.java
@@ -3,8 +3,6 @@ package com.Stiw3054.groupProject;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Properties;
 
 class DisplayPlayerFromKedah {
@@ -20,19 +18,9 @@ class DisplayPlayerFromKedah {
             // load a properties file
             prop.load(input);
             for (int y = 0; y < 10; y++) {
-                for (int x = 0; x < playerlist[1].getArrayRK().length; x++) {
+                for (int x = playerlist[1].getArrayRK().length-1; x>0; x--) { //Sorting winning points in ascending order.
                     if (playerlist[y].getArrayRK()[x] != null) {
                         if ((playerlist[y].getArrayState()[x].equals("KEDAH"))) {
-                            int finalX = x;
-                            Arrays.sort(new String[]{playerlist[y].getArrayPts()[x], String.valueOf(new Comparator<String[]>() {   //* Sorting
-                                @Override
-                                public int compare(String[] o1, String[] o2) {
-                                    Double S1, S2;
-                                    S1 = Double.valueOf(o1[finalX]);
-                                    S2 = Double.valueOf(o2[finalX]);
-                                    return S1.compareTo(S2);
-                                }
-                            })});                                                                                                 //* Sorting
                             System.out.printf("| %-3s  |  %-3s  |  %-42s | %-4s | %-15s  | %-4s | %-50s |\n", playerlist[y].getArrayRK()[x], playerlist[y].getArraySno()[x], playerlist[y].getArrayName()[x], playerlist[y].getArrayRtg()[x], prop.getProperty("sTATE"), playerlist[y].getArrayPts()[x], playerlist[y].getArrayCat()[x]);
                         }
                     }
@@ -43,3 +31,4 @@ class DisplayPlayerFromKedah {
         }
     }
 }
+
